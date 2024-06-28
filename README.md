24e101571030a483b7e4916bf43ab008c0677146
Hixac <darkinbladechadshin92321@gmail.com>
2024-06-28 19:59:24 +0700

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+## Easy compile extension for Emacs
+
+`project-compile` often require either git extension for easy use or manual setting so it's not very handy.
+`compile` is simple as heck it just do command in directory where file belongs so it's not very optional.
+
+So my extension solve these problems.
+
+# Quick start
+
+To use it just do two steps:
+1. `M-x set-compile-directory [directory you want to chose] RET`
+2. `M-x compile-dir [shell-command] RET`
---
+++ defun.el
@@ -0,0 +1,41 @@
+(defun read-file (filepath)
+  (if (file-exists-p filepath)
+	  (with-temp-buffer
+		(insert-file-contents filepath)
+		(buffer-string))
+	(error "No file found!"))
+)
+
+(defun clear-file (filepath)
+  (if (file-exists-p filepath)
+	  (progn
+		(delete-file filepath)
+		(make-empty-file filepath))
+	(make-empty-file filepath)))
+
+(defun initialize-config-file (path)
+  (progn
+	(setq compile-path "~/.emacs.d/HIXAC/compile-path")
+    (clear-file compile-path)
+	(append-to-file path nil compile-path)
+	)
+)
+
+(defun set-compile-directory (dir)
+  (interactive (list (read-directory-name "What directory? " default-directory)))
+   (if (file-directory-p dir)
+		 (initialize-config-file dir)
+	 (message "Can not set this."))
+)
+
+(defun compile-dir (com)
+  (interactive (list (read-shell-command "Compile command: ")))
+  (progn
+	(setq compile-path "~/.emacs.d/HIXAC/compile-path")
+	(if (file-exists-p compile-path)
+	    (progn
+		  (setq path (read-file compile-path))
+		  (shell-command (concat "cd " path " && " com)))
+	  (message "Set directory first!"))
+	)
+)
